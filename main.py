074236d12d92bec4db66ccf56746b1f913b1a9c1
Ayush Sharma <ayushanavsharma2@gmail.com>
2025-08-20 20:00:01 +1200

    added asteroids that randomly spawn and move

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,13 +17,17 @@ def main():
     # obj groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroid_field = pygame.sprite.Group()
     
     # setting group as containers for player obj
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     
     # player obj
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    
+    asteroidfield = AsteroidField()
     
     while True:
         for event in pygame.event.get():
@@ -29,7 +35,7 @@ def main():
                 return
         screen.fill((0,0,0))
         updatable.update(dt)
-        
+
         for draw in drawable:
             draw.draw(screen)
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,49 @@
-datable = pygame.sprite.Group()
+import os
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+def main():
+    pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
+    # obj groups
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    asteroid_field = pygame.sprite.Group()
+    
+    # setting group as containers for player obj
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    
+    # player obj
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0,0,0))
+        updatable.update(dt)
+
+        for draw in drawable:
+            draw.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
+    
+    
+if __name__ == "__main__":
+    main()
+    
